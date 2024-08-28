52aa9393c540a56912cdf62b0def0c9e14457f5f
tanushka-jha23 <tanushkajha670@gmail.com>
2024-08-28 20:04:14 +0530

    fifth commit

--- sorting.c
+++ sorting.c
@@ -31,19 +31,17 @@ void bubble_sort(int* a, int length) {
 }
 
 void bubble_sort_conventional(int* a, int length) {
-  int count = 0;
-  for(int i=0; i < length-1; i++) {
-    if(a[i] > a[i+1]) {
-      count = count + 1;
-    }
-  }
-  if(count != 0) {
-    for(int i = 0; i < length-1; i++) {
-      for(int j = 0; j < length-i-1; j++) {
-        if(a[j] > a[j+1]) {
-          swap(a+j, a+j+1);
-        }
+  int flag = 0; 
+  for(int i = 0; i < length-1; i++) {
+    flag = 0;
+    for(int j = 0; j < length-i-1; j++) {
+      if(a[j] > a[j+1]) {
+        flag = 1;
+        swap(a+j, a+j+1);
       }
+    }
+    if(flag == 0) {
+      break;
     }
   }
 }
