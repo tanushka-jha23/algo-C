3befe2d821fae3f3c9b6480f2f2b67f11853cea4
tanushka-jha23 <tanushkajha670@gmail.com>
2024-08-28 18:48:26 +0530

    fourth commit

--- sorting.c
+++ sorting.c
@@ -2,23 +2,31 @@
 #include <stdlib.h>
 
 void swap(int* a, int* b) {
-  int c = *b;
+  int c = *a;
   *a = *b;
   *b = c;
  }
 
 void bubble_sort(int* a, int length) {
-  int i = 0;
-  int j = 1;
-  while(i < length - 1) {
-    while(j < length) {
-      if(a[i] > a[j]) { 
-        swap(a+i, a+j);
-      }
-      j = j + 1;
+  int count = 0;
+  for(int i=0; i < length-1; i++) {
+    if(a[i] > a[i+1]) {
+      count = count + 1;
     }
-    i = i + 1;
-    j = i + 1;
+  }
+  if(count != 0) {
+    int i = 0;
+    int j = 1;
+    while(i < length - 1) {
+      while(j < length) {
+        if(a[i] > a[j]) { 
+          swap(a+i, a+j);
+        }
+        j = j + 1;
+      }
+      i = i + 1;
+      j = i + 1;
+    }
   }
 }
 
@@ -32,17 +40,4 @@ void bubble_sort_conventional(int* a, int length) {
   }
 }
 
-void array_print(int* a, int length) {
-  for(int i = 0; i < length; i++) {
-    printf("%d", a[i]);
-  }
-}
-
-int main() {
-  int arr[6] = {4, 3, 1, 2, 5, 7};
-  bubble_sort_conventional(arr, 6);
-  array_print(arr, 6);
-
-  return 0;
-}
 
